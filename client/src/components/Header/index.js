17a4a9df82ff272b27c05c94389ef827239dd782
Travis <travisbly@gmail.com>
2022-06-22 17:26:14 -0700

    edit to header

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import LoginFormModal from '../auth/LoginFormModal';
+import './Header.css';
+
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Avatar from '@mui/material/Avatar';
+
+
+function Header() {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+          alt=""
+        />
+      </Link>
+
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+// function Header({ isLoaded }) {
+//   const sessionUser = useSelector((state) => state.session.user);
+
+//   let sessionLinks;
+//   if (sessionUser) {
+//     sessionLinks = <ProfileButton user={sessionUser} />;
+//   } else {
+//     sessionLinks = (
+//       <>
+//         <LoginFormModal />
+//         <NavLink to="/signup">Sign Up</NavLink>
+//       </>
+//     );
+//   }
+
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink exact to="/">
+//           Home
+//         </NavLink>
+//         {isLoaded && sessionLinks}
+//       </li>
+//     </ul>
+//   );
+// }
+
+export default Header;
