d6993f1f08d81b56d664735f9044dca20744bd66
Travis <travisbly@gmail.com>
2022-07-03 16:35:48 -0700

    added events hodler

---
+++ client/src/components/EventsHolder/EventsHolder.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import './EventsHolder.css';
+
+const EventsHolder = () => {
+  return <div>EventsHolder</div>;
+};
+
+export default EventsHolder;
