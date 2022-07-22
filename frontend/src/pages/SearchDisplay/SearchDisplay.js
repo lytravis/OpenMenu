10bf8aa0b9dec6c232ad92b45a7680ab238cde53
Travis <travisbly@gmail.com>
2022-07-22 16:05:23 -0700

    working on search display mapping

--- frontend/src/pages/SearchDisplay/SearchDisplay.js
+++ frontend/src/pages/SearchDisplay/SearchDisplay.js
@@ -12,13 +12,22 @@ const SearchDisplay = () => {
   const events = useSelector((state) => Object.values(state.event));
   const searchResults = useSelector((state) => Object.values(state?.search));
 
-  console.log('%%%%%%%%%%%%%', searchTerm);
+  const searchEvents = searchResults[0]
+
+  console.log('!!!!!!!!!!!!!!!!!!!! searchEvents', searchEvents);
+
+  // const tester = searchEvents.map((event) => console.log(event.name));
+
+  // console.log('!!!!!!!!!!!!!!!!!!!!!!!!TESTER', tester);
+
+  // console.log('%%%%%%%%%%%%% search Term', searchTerm);
+  // console.log('%%%%%%%%%%%%% search results', searchResults);
   // console.log('***********Your Events', events);
 
   const [isLoaded, setIsLoaded] = useState(true);
-  const [searchString, setSearchString] = useState(`${searchTerm}`);
+  // const [searchString, setSearchString] = useState(`${searchTerm}`);
 
-  console.log('***********Your searchString', searchString);
+  // console.log('***********Your searchString', searchString);
 
   useEffect(() => {
     dispatch(getEvents()).then(() => setIsLoaded(true));
@@ -28,10 +37,8 @@ const SearchDisplay = () => {
   }, [dispatch]);
 
   useEffect(() => {
-    if (searchString !== '') {
-      dispatch(searchResultsType(searchTerm));
-    }
-  }, [dispatch, searchString, searchTerm]);
+    dispatch(searchResultsType(searchTerm));
+  }, [dispatch, searchTerm]);
 
   useEffect(() => {
     document.title = `Search · OpenMenu`;
@@ -44,20 +51,20 @@ const SearchDisplay = () => {
     <>
       {isLoaded && (
         <div className="search-container">
-          {events?.length > 0 && events[0] !== null && (
+          {searchEvents?.length > 0 && searchEvents[0] !== null && (
             <div
               className="search-dogsInfo-map-container"
               id="searchResultHeader"
             >
               <div className="search-EventList">
                 <div className="search-buttons">
-                  {events?.length === 1 ? (
+                  {searchEvents?.length === 1 ? (
                     <h1>{`Search Results - 1 event`}</h1>
                   ) : (
-                    <h1>{`Search Results - ${events?.length} events`}</h1>
+                    <h1>{`Search Results - ${searchEvents?.length} events`}</h1>
                   )}
                 </div>
-                {events?.map((event) => (
+                {searchEvents?.map((event) => (
                   <div
                     className="search-eventContainer"
                     key={`Event_holder_${event?.id}`}
