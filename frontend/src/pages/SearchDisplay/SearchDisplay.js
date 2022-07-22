27afaaca13f86a542175de8b18285ea5a4be5cf7
Travis <travisbly@gmail.com>
2022-07-22 13:51:18 -0700

    added search page

---
+++ frontend/src/pages/SearchDisplay/SearchDisplay.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+const SearchDisplay = () => {
+  return <div>SearchDisplay</div>;
+};
+
+export default SearchDisplay;
